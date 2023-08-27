46ee52ca99cce69c631a265e701d0adf7beebedb
Piotr Trehubovich <petertregubovich@gmail.com>
2023-08-27 14:53:07 +0200

    init: changes

---
+++ src/components/Header.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import logo from "../assets/img/North_S_5.png";
+
+const Header = () => {
+  return (
+    <header>
+      <>
+        <img src={logo} alt="logo"/>
+      </>
+      <>
+        
+      </>
+    </header>
+  )
+}
+
+export default Header
